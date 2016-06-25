60b9d6ed8bdef3702b787fc7a40d2bfec915d36b
PHP - MySQL <PHP - MySQL>
2016-06-25 11:52:30 -0300

    Classes controller

---
+++ control/UsuarioControl.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UsuarioControl
+ *
+ * @author PHP - MySQL
+ */
+class UsuarioControl {
+    public function inserir() {
+        
+    }
+    
+    public function listarTodos(){
+        
+    }
+    
+    public function buscar(){
+        
+    }
+    
+    public function editar(){
+        
+    }
+    
+    public function deletar(){
+        
+    }
+}
