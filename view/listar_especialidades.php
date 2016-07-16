d7a4d680f821bf4aace70027f38f199a370f309a
PHP - MySQL <PHP - MySQL>
2016-07-16 12:36:35 -0300

    menu bar e estilização em listagem de especialidades

--- view/listar_especialidades.php
+++ view/listar_especialidades.php
@@ -1,21 +1,38 @@
 <?php
+include_once '../style/template.php';
 include_once '../control/EspecialidadeControl.php';
 $especialidadeControl = new EspecialidadeControl();
 
 //print_r($especialidadeControl->listarTodos());
 ?>
-
-<table >
-    <tr>
-        <th>id</th>
-        <th>nome</th>
-    </tr>
-   <?php 
-    foreach ($especialidadeControl->listarTodos() as $especialidade) {
-       ?>
+<div class="col-sm-10 col-sm-offset-1">
+    
+    
+    <h1>Especialidades</h1>
+    
+    <a href="" class="pull-right btn btn-primary btn-xs">cadastrar especialidade</a>
+    
+    <table class="table table-hover" >
         <tr>
-            <td><?php echo $especialidade->id; ?></td>
-            <td><?php echo $especialidade->nome; ?></td>
+            <th>id</th>
+            <th>nome</th>
+            <th></th>
         </tr>
-   <?php } ?>
-</table>
+       <?php 
+        foreach ($especialidadeControl->listarTodos() as $especialidade) {
+           ?>
+            <tr>
+                <td><?php echo $especialidade->id; ?></td>
+                <td><?php echo $especialidade->nome; ?></td>
+                <td>
+                    <div class="pull-right">
+                        <a href="">editar</a> 
+                        |
+                        <a href="">deletar</a>
+                    </div>
+                </td>
+            </tr>
+       <?php } ?>
+    </table>
+</div>
+<?php include_once '../style/footer.php'; ?>
